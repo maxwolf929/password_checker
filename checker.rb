eb36bb0ce12d0d9ae4cdf36ea61176332bb10eb9
maxwolf <maxwolf929@gmail.com>
2014-03-16 13:01:00 +0200

    First commit

---
+++ checker.rb
@@ -0,0 +1,20 @@
+﻿require 'sinatra'
+
+get "/" do
+	erb :index
+end
+
+post "/show_result" do
+	@p = /[\@\#\_\-]/.match(params[:your_password])
+	
+	@result = if @p
+		"Strong Password!"
+	else	
+		"Weak Password!"
+	end
+
+	erb :show_result
+end
+
+#erb :show_result, :locals => {:s => params[:s]}
+#erb :show_result, :locals => {:w => params[:w]}
---
+++ config.ru
@@ -0,0 +1,2 @@
+require "./checker"
+run Sinatra::Application
