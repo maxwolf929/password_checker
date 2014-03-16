80cfaf2c67230a809136b0ce365b5acc397b94b2
maxwolf <maxwolf929@gmail.com>
2014-03-16 15:17:05 +0200

    GNU License

--- checker.rb
+++ checker.rb
@@ -1,4 +1,20 @@
-﻿require 'sinatra'
+﻿=begin
+<Password Strenght Checker>
+    Copyright (C) <2014>  <Sherif Abdelhay>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+require 'sinatra'
 
 get "/" do
 	erb :index
